576532178133394c28233adbf30f80440ae47aa4
Adrián Robles <adrian.robles@scmspain.com>
2017-03-15 17:09:02 +0100

    Add exercise 2

---
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_2.kt
@@ -0,0 +1,13 @@
+package io.jimmyoak.kotlinworkshop.exercises
+
+/*
+
+Write a program which converts data() into a string (ints to its ascii representation)
+
+Write a program which converts data2() applying ROT-13 and outputs the result as a String (control only from A to Z)
+
+*/
+
+fun data() = listOf(106, 105, 109, 109, 121)
+
+fun data2() = "WVZZL"
