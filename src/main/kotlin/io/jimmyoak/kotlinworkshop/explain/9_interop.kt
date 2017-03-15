348ffd1413390779333aecd692309ef268bc3587
Adrián Robles <adrian.robles@scmspain.com>
2017-03-15 16:09:27 +0100

    some changes

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/explain/9_interop.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/explain/9_interop.kt
@@ -1,11 +1,26 @@
 package io.jimmyoak.kotlinworkshop.explain
 
+import java.math.BigInteger
 import java.util.*
 import kotlin.concurrent.schedule
 import kotlin.concurrent.scheduleAtFixedRate
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    bigInteger()
+    scheduling()
+}
+
+private fun bigInteger() {
+    val a = BigInteger.valueOf(5)
+    val b = BigInteger.ONE
+
+    val c = a + b
+
+    println(c)
+}
+
+private fun scheduling() {
     val timer = Timer()
 
     val task = timer.scheduleAtFixedRate(0, 1000) {
