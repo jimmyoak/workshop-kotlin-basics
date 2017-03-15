c61bdfed0d16c569762f26bafaaf0dee930bef92
Adrián Robles <adrian.robles@scmspain.com>
2017-03-15 17:20:15 +0100

    Add exercise 3

---
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
@@ -0,0 +1,13 @@
+package io.jimmyoak.kotlinworkshop.exercises
+
+/*
+Make a class Order
+Order has order lines
+Order lines have product name, quantity and line total
+Order has VAT of 21%
+Order has total
+
+I order a PS4 (300€), a Resident Evil 7(70€) and a VR(400€)
+
+Print lines and total
+*/
