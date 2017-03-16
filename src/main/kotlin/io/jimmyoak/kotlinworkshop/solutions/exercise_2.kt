a7abbbbe9d8d464fdac0a6d6815092441dd28516
Adrián Robles <adrian.robles@scmspain.com>
2017-03-16 11:12:11 +0100

    add exercise_3 solution

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/solutions/exercise_2.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/solutions/exercise_2.kt
@@ -2,9 +2,11 @@ package io.jimmyoak.kotlinworkshop.solutions
 
 /*
 
-Write a program which converts data() into a string (ints to its ascii representation) and outputs de result as String
+Write a program which converts data() into a string (ints to its ascii representation)
 
 Write a program which converts data2() applying ROT-13 and outputs the result as a String (control only from A to Z)
+
+Write a program which fins prime numbers between 0 and 50 and sums them
 
 */
 
@@ -14,7 +16,12 @@ fun data2() = "WVZZL"
 
 fun rot13(letter: Char) = if (letter in 'A'..'M') (letter.toInt() + 13).toChar() else (letter.toInt() - 13).toChar()
 
+fun Int.isPrime() = (2 until this).filter { this % it == 0 }.isEmpty()
+
+fun primeNumbersSum() = (1..50).filter(Int::isPrime).sum()
+
 fun main(args: Array<String>) {
     println(data().map(Int::toChar).joinToString(""))
     println(data2().map(::rot13).joinToString(""))
+    println(primeNumbersSum())
 }
---
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/solutions/exercise_3.kt
@@ -0,0 +1,55 @@
+package io.jimmyoak.kotlinworkshop.solutions
+
+/*
+Make a class Order
+Order has order lines
+Order lines have product name, quantity and line total
+Order has VAT of 21%
+Order has total
+
+I order a PS4 (300€), a Resident Evil 7(70€) and a VR(400€)
+
+Print lines and total
+*/
+
+
+class Order {
+    private val lines: MutableList<OrderLine> = mutableListOf()
+
+    fun addOrderLine(product: String, quantity: Int, price: Double) = lines.add(OrderLine(product, quantity, price))
+
+    fun lines(): List<OrderLine> = lines
+
+    fun total() = lines.map(OrderLine::total).sum()
+}
+
+data class OrderLine(val product: String, val quantity: Int, val price: Double) {
+    fun total() = quantity * price
+}
+
+object OrderPrinter {
+    fun print(order: Order) {
+        printLine("Product", "Quantity", "Price", "Total")
+        println("-".repeat(60))
+        order.lines().forEach(OrderPrinter::printLine)
+        println("Total: ${order.total().toString().padStart(52)}")
+    }
+
+    private fun printLine(orderLine: OrderLine) {
+        printLine(orderLine.product, orderLine.quantity.toString(), orderLine.price.toString(), orderLine.total().toString())
+    }
+
+    private fun printLine(product: String, quantity: String, price: String, total: String) {
+        println("${product.padEnd(20)} | ${quantity.padStart(10)} | ${price.padStart(10)} | ${total.padStart(10)}")
+    }
+}
+
+fun main(args: Array<String>) {
+    val order = Order()
+    order.addOrderLine("PS4", 1, 299.95)
+    order.addOrderLine("DualShock 4", 2, 59.95)
+    order.addOrderLine("Resident Evil 7", 1, 69.95)
+    order.addOrderLine("VR", 1, 399.95)
+
+    OrderPrinter.print(order)
+}
