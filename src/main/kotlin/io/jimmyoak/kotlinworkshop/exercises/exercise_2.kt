5c3156857877f492393496faee26d1c01c460bef
Xavier Fornes <xavier.fornes@scmspain.com>
2017-03-17 13:45:30 +0100

    cosas

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_2.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_2.kt
@@ -13,3 +13,28 @@ Write a program which fins prime numbers between 0 and 50 and sums them
 fun data() = listOf(106, 105, 109, 109, 121)
 
 fun data2() = "WVZZL"
+
+fun main(args: Array<String>) {
+    val string = data()
+            .map(Int::toChar)
+            .joinToString("")
+    println(string)
+
+
+    (1..50)
+            .filter(::isPrime)
+            .forEach(::println)
+
+    val sum = (1..50)
+            .filter(::isPrime)
+            .sum()
+    println(sum)
+
+}
+
+fun isPrime(number: Int): Boolean {
+    return (1..number/2)
+            .filter { number % it == 0 }
+            .count() <= 1
+}
+
--- src/main/kotlin/io/jimmyoak/kotlinworkshop/explain/3_lambdas.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/explain/3_lambdas.kt
@@ -3,15 +3,16 @@ package io.jimmyoak.kotlinworkshop.explain
 typealias Stringifier = (Int) -> String
 
 fun stringify(number: Int, stringifier: Stringifier) = stringifier(number).toLowerCase().trim()
+fun stringifyWithoutAlias(number: Int, action: (Int) -> String) = stringify(number, action)
 
 fun main(args: Array<String>) {
     val callback: Stringifier = {
         limit -> (0..limit)
-            .map { "Hi" }
+            .map { "Hi $it" }
             .joinToString()
     }
 
-    val b = stringify(5, callback)
+    val b = stringifyWithoutAlias(5, callback)
 
     println(b)
 
