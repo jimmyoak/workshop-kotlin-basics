0c9f07af91dd681f990ad938087a1d4dba1289f9
bindme <bindme.net@gmail.com>
2017-03-24 12:43:11 +0100

    Finally excercise 3 change var name

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
@@ -12,20 +12,19 @@ I order a PS4 (300€), a Resident Evil 7(70€) and a VR(400€)
 Print lines and total
 */
 
-class Order(var productName: String, var quantity: Int, var price: Double)
+class OrderLine(var productName: String, var quantity: Int, var price: Double)
 
 fun main(args: Array<String>) {
-
     var vat: Double = 0.21
     var totalOrdersPrice: Double = 0.00;
-    var totalOrdersPriceVat: Double = 0.00;
+    var totalVAT: Double = 0.00;
 
-    var orderPS4: Order = Order("PS4", 1, 299.95)
-    var orderResident: Order = Order("Resident Evil", 1, 69.95)
-    var orderDualShock: Order = Order("DualShock 4", 2, 59.95)
-    var orderVR: Order = Order("VR", 1, 399.95)
+    var orderPS4: OrderLine = OrderLine("PS4", 1, 299.95)
+    var orderResident: OrderLine = OrderLine("Resident Evil", 1, 69.95)
+    var orderDualShock: OrderLine = OrderLine("DualShock 4", 2, 59.95)
+    var orderVR: OrderLine = OrderLine("VR", 1, 399.95)
 
-    val orders: List<Order> = listOf(orderPS4, orderDualShock, orderResident, orderVR)
+    val orders: List<OrderLine> = listOf(orderPS4, orderDualShock, orderResident, orderVR)
 
     orders
             .forEach{
@@ -33,7 +32,7 @@ fun main(args: Array<String>) {
                 println(it.productName + " - " + it.price)
             }
 
-    totalOrdersPriceVat = totalOrdersPrice * vat
-    println("Total: " + (totalOrdersPrice - totalOrdersPriceVat))
+    totalVAT = totalOrdersPrice * vat
+    println("Total: " + (totalOrdersPrice - totalVAT))
 
 }
