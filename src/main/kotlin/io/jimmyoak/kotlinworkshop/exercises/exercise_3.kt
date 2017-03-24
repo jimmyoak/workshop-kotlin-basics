5d1b8abf35b301b1cdca7e64d8489cfb0da7f921
jose fernandez <jose.fernandez@scmspain.com>
2017-03-24 13:13:50 +0100

    exercice 3

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
@@ -3,11 +3,47 @@ package io.jimmyoak.kotlinworkshop.exercises
 /*
 Make a class Order
 Order has order lines
-Order lines have product name, quantity and line total
+Order lines have product name, quantity and line getTotal
 Order has VAT of 21%
-Order has total
+Order has getTotal
 
 I order a PS4 (300€), a Resident Evil 7(70€) and a VR(400€)
 
-Print lines and total
+Print lines and getTotal
 */
+class Game (val name: String, val price: Double)
+class OrderLine (val game: Game, val quantity: Int){
+    fun getTotal() : Double = game.price * quantity
+}
+class Order{
+    private val Vat = 0.21
+    private val orderLines = mutableListOf<OrderLine>()
+    fun getOrderLines() : List<OrderLine>  = orderLines
+    fun addOrderLine(orderLine : OrderLine) = orderLines.add(orderLine)
+    fun getTotal() : Double {
+        var total = 0.0
+        for (orderLine in orderLines) {
+            total += orderLine.getTotal()
+        }
+        return total
+    }
+}
+class PrinterLine(){
+    fun printLine(orderLine: OrderLine) = "$orderLine.game.name $orderLine.game.price $orderLine.quantity $orderLine.getTotal"
+}
+fun main(args: Array<String>) {
+    var order = Order()
+    val printerLine = PrinterLine()
+    order.addOrderLine(OrderLine(Game("PS4",299.95),1))
+    order.addOrderLine(OrderLine(Game("DualShock 4",59.95),2))
+    order.addOrderLine(OrderLine(Game("Resident Evil 7",69.95),1))
+    order.addOrderLine(OrderLine(Game("VR",399.95),1))
+
+    println(order.getOrderLines()
+            .map {
+                printerLine.printLine(it)
+            }
+            .joinToString(" "))
+
+
+}
