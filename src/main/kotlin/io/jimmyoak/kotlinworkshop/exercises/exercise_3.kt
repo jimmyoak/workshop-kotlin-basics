63a1244a204b9ae015ad291ff162c1bc444197f9
bindme <bindme.net@gmail.com>
2017-03-24 12:19:01 +0100

    Finally excercise 3

--- src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
+++ src/main/kotlin/io/jimmyoak/kotlinworkshop/exercises/exercise_3.kt
@@ -11,3 +11,24 @@ I order a PS4 (300€), a Resident Evil 7(70€) and a VR(400€)
 
 Print lines and total
 */
+
+class Order(var productName: String, var quantity: Int, var price: Double)
+
+fun main(args: Array<String>) {
+
+    var orderPS4: Order = Order("PS4", 1, 300.00)
+    var orderResident: Order = Order("Resident Evil", 1, 70.00)
+    var orderVR: Order = Order("VR", 1, 400.00)
+
+    val orders: List<Order> = listOf(orderPS4, orderResident, orderVR)
+    var totalOrders: Double = 0.00;
+
+    orders
+            .forEach{
+                totalOrders += it.price
+                println(it.productName + " - " + it.price.toString())
+            }
+
+            println("Total: " + totalOrders)
+
+}
